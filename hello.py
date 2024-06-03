44bc224e366c15fed433e2fe81a4bb6b21242ebf
J0rgit000 <jorgemooolina@gmail.com>
2024-06-03 10:28:08 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,7 @@
 def hello_world():
-    print('"Hello World!"')
+    print('Hello World!')
     
 def hello_world_n(N):
-    s='"'
-    string='Hello World! '*N
-    print('"'+'Hello World! '*N+'"')
+    print('Hello World! '*N)
 hello_world()
 hello_world_n(3)
